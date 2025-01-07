0b7bd52e4ad316b11546c0357c46a1f7acf84948
shir shponder <shponders@gmail.com>
2025-01-07 23:01:51 +0200

    add and generate recycler view list

--- app/src/main/java/com/example/mobile_application_course/MainActivity.kt
+++ app/src/main/java/com/example/mobile_application_course/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.mobile_application_course
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,9 +15,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.colman24classandroid.model.Model
 import com.example.colman24classandroid.model.Student
+import com.example.mobile_application_course.Interfaces.OnItemClickListener
+
 
 class MainActivity : AppCompatActivity() {
 
+    var students: MutableList<Student>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +32,91 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        students = Model.shared.students
 
+        val recyclerView: RecyclerView =
+            findViewById(R.id.students_list_fragment_recycler_view)
+
+        recyclerView.setHasFixedSize(true)
+
+        val layoutManger = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManger
+
+        val adapter = StudentsRecyclerAdapter(students)
+
+        adapter.listener = object : OnItemClickListener {
+            override fun onItemClick(student: Student?) {
+                Log.d("TAG", "On student clicked name: ${student?.name}")
+            }
+        }
+
+        recyclerView.adapter = adapter
+
+    }
+
+    class StudentViewHolder(itemView: View, listener: OnItemClickListener?) :
+        RecyclerView.ViewHolder(itemView) {
+        private var nameTextView: TextView? = null
+        private var idTextView: TextView? = null
+        private var checkBox: CheckBox? = null
+        private var student: Student? = null
+
+        init {
+            nameTextView = itemView.findViewById(R.id.student_row_name_text_view)
+            idTextView = itemView.findViewById(R.id.student_row_id_text_view)
+            checkBox = itemView.findViewById(R.id.student_row_check_box)
+
+            checkBox?.apply {
+                setOnClickListener { view ->
+                    (tag as? Int)?.let {
+                        student?.isChecked = (view as? CheckBox)?.isChecked ?: false
+                    }
+                }
+            }
+
+            itemView.setOnClickListener {
+                listener?.onItemClick(student)
+            }
+        }
+
+        fun bind(student: Student?, position: Int) {
+            this.student = student
+            nameTextView?.text = student?.name
+            idTextView?.text = student?.id
+
+            checkBox?.apply {
+                isChecked = student?.isChecked ?: false
+                tag = position
+            }
+        }
+    }
+
+    class StudentsRecyclerAdapter(private val students: List<Student>?) :
+        RecyclerView.Adapter<StudentViewHolder>() {
+
+        var listener: OnItemClickListener? = null
+
+
+        override fun getItemCount(): Int = students?.size ?: 0
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+
+            val view =
+                inflater.inflate(
+                    R.layout.student_list_row,
+                    parent,
+                    false
+                )
+
+
+            //this function create a new view, like before
+            return StudentViewHolder(view, listener)
+        }
+
+        override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+            // insert the data of every row
+            holder.bind(students?.get(position), position)
+        }
     }
 }
